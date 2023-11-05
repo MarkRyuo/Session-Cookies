35b31056671cc8fa8dbe36d545d32345b2c311cc
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-11-05 21:08:43 +0800

    update - 3

--- PHP SESSIONS And Cookiess/Php/SESSION/Session.php
+++ PHP SESSIONS And Cookiess/Php/SESSION/Session.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Session</title>
-</head>
-<h1> Assign - Session </h1>
-<body>
-<?php
-// Start a session
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Session</title>
+    </head>
+    <h1> Assign - Session </h1>
+    <body>
+    <?php
+    // Start a session
 
-// Start a new session or resume the existing session
-session_start();
+    // Start a new session or resume the existing session
+    session_start();
 
-// Set session variables
+    // Set session variables
 
-// Assign values to session variables
-$_SESSION["userId"] = 19842;
-$_SESSION["userName"] = "ODLANYER R. LIAQUEN";
-$_SESSION["Age"] = "20";
+    // Assign values to session variables
+    $_SESSION["userId"] = 19842;
+    $_SESSION["userName"] = "Malupa Jhon Mark L.";
+    $_SESSION["Age"] = "20";
 
-// Display a message to confirm that the session variables have been set
-echo "Session variables have been set.";
-?>
-</body>
-</html>
+    // Display a message to confirm that the session variables have been set
+    echo "Session variables set successfully.";
+    ?>
+    </body>
+    </html>
